d5fbb25afc95b10486f95b7c926b8a7a146d5fd9
Devilpacks <mangyr97@mail.ru>
2022-04-25 20:42:10 +0300

    Go 1

--- gotour/hello.go
+++ gotour/hello.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+var c, python, java bool
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println("Hello, World! \n")
+    fmt.Println(math.Pi)
+    fmt.Println(add(42, 13))
+    a, b := swap("hello", "world")
+	fmt.Println(a, b)
+    fmt.Println(split(17))
+    var i int
+	fmt.Println(i, c, python, java)
 }
+
