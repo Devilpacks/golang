de322739eddbb84ad84f0b03420ed9f4277ea53d
Devilpacks <mangyr97@mail.ru>
2022-05-05 18:33:34 +0300

    shortAssignment

--- gotour/hello.go
+++ gotour/hello.go
@@ -29,6 +29,16 @@ func main() {
 	fmt.Println(a, b)
     fmt.Println(split(17))
     var i, j int = 1, 2
-	fmt.Println(i, c, python, java)
+	fmt.Println(i, j, c, python, java)
 }
 
+
+// Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type.
+// Outside a function, every statement begins with a keyword (var, func, and so on) and so the := construct is not available.
+func shortAssignment() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "nooooooooooooooooo!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
