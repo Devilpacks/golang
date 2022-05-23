eb268f6533fbb91d87dc04bc24f9f1a45d7d91db
Devilpacks <mangyr97@mail.ru>
2022-05-23 18:51:48 +0300

    type Conversions

--- gotour/hello.go
+++ gotour/hello.go
@@ -41,6 +41,9 @@ func zeroValues() {
 
 func typeConversions() {
 	// The expression T(v) converts the value v to the type T.
+	// i := 42
+	// f := float64(i)
+	// u := uint(f)
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x*x + y*y))
 	var z uint = uint(f)
